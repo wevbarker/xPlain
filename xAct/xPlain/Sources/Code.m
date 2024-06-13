895197c83d0984c9cacc7077065ca4467e06a57c
William Barker <wb263@cam.ac.uk>
2024-06-13 16:23:04 +0100

    removing quoted line number in code

--- xAct/xPlain/Sources/Code.m
+++ xAct/xPlain/Sources/Code.m
@@ -23,7 +23,7 @@ LstListingCode[InputExpr__]:=Block[{
 			FileNameJoin@{$xPlainWorkingDirectory,"LstListing",$ListingsOutput<>".tex"},
 			PageWidth->Infinity];
 
-	WriteString[ListingsFile,"In[\#]:= "<>FormattedInput<>""];
+	WriteString[ListingsFile,"In[#]:= "<>FormattedInput<>""];
 	(*WriteString[ListingsFile,"In["<>ToString@$LstListingsLine<>"]:= "<>FormattedInput<>""];*)
 	Close@ListingsFile;
 ];
